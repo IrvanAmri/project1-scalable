201500e863f2215e249fa8256d0629e45ca632da
IrvanAmri <72341169+IrvanAmri@users.noreply.github.com>
2022-04-11 23:54:39 +0700

    Create microservice: StringToBlock

---
+++ string-to-block/src/test/java/com/grup4/stringtoblock/StringToBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grup4.stringtoblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringToBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

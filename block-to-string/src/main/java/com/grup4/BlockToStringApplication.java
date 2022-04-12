a9d8e747b468711fd973f583496a122d7e9c85f3
IrvanAmri <72341169+IrvanAmri@users.noreply.github.com>
2022-04-12 12:29:37 +0700

    create microservice: BlockToString

---
+++ block-to-string/src/main/java/com/grup4/BlockToStringApplication.java
@@ -0,0 +1,13 @@
+package com.grup4.blocktostring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockToStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockToStringApplication.class, args);
+	}
+
+}
---
+++ block-to-string/src/test/java/com/grup4/blocktostring/BlockToStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grup4.blocktostring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockToStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

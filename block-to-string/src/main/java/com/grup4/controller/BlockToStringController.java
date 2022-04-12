842877b9b143cdd6fb15b8f2a06f4660120ec0d0
IrvanAmri <72341169+IrvanAmri@users.noreply.github.com>
2022-04-12 14:10:27 +0700

    update BlockToString

--- block-to-string/src/main/java/com/grup4/controller/BlockToStringController.java
+++ block-to-string/src/main/java/com/grup4/controller/BlockToStringController.java
@@ -1,5 +1,24 @@
 package com.grup4.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.grup4.service.BlockToStringService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/text")
 public class BlockToStringController {
     
+    @Autowired
+    private BlockToStringService service;
+
+    @GetMapping
+    public String getString(@RequestParam(name="json") String json) throws JsonMappingException, JsonProcessingException{
+        return service.service(json);
+    }
 }
--- block-to-string/src/main/java/com/grup4/service/BlockToStringService.java
+++ block-to-string/src/main/java/com/grup4/service/BlockToStringService.java
@@ -1,5 +1,20 @@
 package com.grup4.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.grup4.helper.Helper;
+import com.grup4.model.repo.BlockRepo;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class BlockToStringService {
     
+    public String service(String json) throws JsonMappingException, JsonProcessingException{
+        //to block repo
+        BlockRepo blockRepo = Helper.jsonToBlockRepo(json);
+
+        //invers mapping
+        return Helper.inverseMapping(blockRepo);
+    }
 }
